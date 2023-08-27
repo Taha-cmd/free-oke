d2c8fbc786cf6fcb672750849453541396352274
Taha <taharaed98@gmail.com>
2023-08-27 15:58:57 +0200

    Add Deployment script and example tfvars files

---
+++ Deploy-Module.ps1
@@ -0,0 +1,11 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)] [ValidateSet("oke", "k8s")] [string] $Module,
+    [Parameter()] [string] [ValidateSet("TRACE", "DEBUG", "INFO", "WARN", "ERROR")] $LogLevel = "ERROR"
+)
+
+$private:sharedDir = Join-Path $PSScriptRoot "Shared"
+$private:modulePath = Join-path $PSScriptRoot $Module
+
+$env:TF_LOG = $LogLevel
+terraform -chdir="$modulePath" apply -var-file="$sharedDir/oci.tfvars" -var-file="$modulePath/values.tfvars"
